a5b92107224dd7cbbaae9a528a8b0d7db9591a03
Rúben Maia <maia.ruben@gmail.com>
2019-09-18 12:50:38 +0200

    changes

---
+++ banner.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const pkg = require("./package.json");
+const filename = "assets/js/main.min.js";
+const script = fs.readFileSync(filename);
+const padStart = str => ("0" + str).slice(-2);
+const dateObj = new Date();
+const date = `${dateObj.getFullYear()}-${padStart(
+  dateObj.getMonth() + 1
+)}-${padStart(dateObj.getDate())}`;
+const banner = `/*!
+ * Personal Jekyll Blog Theme ${pkg.version} by ${pkg.author}
+ * Copyright 2019-${dateObj.getFullYear()} Ruben Maia | * Licensed under ${pkg.license}
+ */
+`;
+
+if (script.slice(0, 3) != "/**") {
+  fs.writeFileSync(filename, banner + script);
+}
